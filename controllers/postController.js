9990b329f40beae13f2f3b261b4fcfa639eb3e41
LorenzoScalvini <lorenzo.scalvini1704@gmail.com>
2024-12-20 15:34:12 +0100

    ADD: gestione dei router con postcontroller.js

---
+++ controllers/postController.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const postController = require("../controllers/postController");
+
+router.get("/posts", postController.getAllPosts);
+
+router.get("/posts/:id", postController.getPostById);
+
+router.delete("/posts/:id", postController.deletePostById);
+
+module.exports = router;
